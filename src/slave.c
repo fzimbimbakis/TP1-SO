bb42c0fad254c2d42ecf10b00b26762b22a14dfb
bsquillari <bsquillari@itba.edu.ar>
2021-09-08 16:33:45 -0300

    Slave completo

--- src/slave.c
+++ src/slave.c
@@ -1,14 +1,42 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char const *argv[])
 {
-    char* string = NULL;
-    int n=0;
-    printf("Texto Slave\n");
-    char c;
-    getline(&string, &n , stdin);
-    printf(string);
-    return 0;
+     fclose(stderr);
+     
+            char *minisatLine = malloc(256);
+           char *command = malloc(256);
+           
+           char *filePath;
+           size_t len;
+           ssize_t read;
+    
+    while (1) {
+                
+                filePath=NULL;
+                len=0;
+               read = getline(&filePath, &len, stdin);
+    
+               if(read==0 || read==-1){
+                   free(filePath);
+                   return read;
+               }
+
+                strcpy(command, "minisat ");
+                command = strncat(command, filePath, 256);
+
+                FILE *minisatFilePipe = popen(command, "r");
+                FILE *grepFilePipe = popen("grep -o \"UN.*\"", "w");
+                
+                
+                while(fscanf(minisatFilePipe, "%s\n", minisatLine)!=EOF){
+                    fprintf(grepFilePipe, "%s\n", minisatLine);
+                }
+
+                fclose(minisatFilePipe);
+                fclose(grepFilePipe);
+    }     
+        return 0;
 }
